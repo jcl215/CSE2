d6c07275c1bc52f38564d6a30ad9574e6f5945cb
jcl215 <jcl215@lehigh.edu>
2014-08-31 18:19:18 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,14 @@
+public class WelcomeClass{
+    public static void main(String[] args) {
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^"); 
+        System.out.println("// \\// \\// \\// \\// \\//");
+        System.out.println("|-J--C--L--2--1--5-|"); 
+        System.out.println("\\ //\\ //\\ //\\ //\\ //\\");
+        System.out.println("  v  v  v  v  v  v"); 
+        System.out.println("I'm Jessica & I'm an Industrial & Systems Engineering major");
+        
+    }
+}
