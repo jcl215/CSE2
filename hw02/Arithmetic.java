5a514561d67367f51800de1f036eda0c51f280bf
jcl215 <jcl215@lehigh.edu>
2014-09-10 02:21:54 +0000

    updated Arithmetic

---
+++ hw02/Arithmetic.java
@@ -0,0 +1,53 @@
+Jessica Lazo Quintana
+
+// define a class
+public class Arithmetic{
+
+//  add main method
+  public static void main(String[] args) {
+        
+        //Number of pairs of socks
+        int nSocks=3;
+        //Cost per pair of socks
+        //(‘$’ is part of the variable name)
+        double sockCost$=2.58;
+        double sockCostbeforetax$=2.58*3; //sock cost before tax
+        double sockcostwithtax$=2.58*.06; //sock cost with tax
+        System.out.println("sockcostwithtax$ and sockCostbeforetax$);
+        
+        //Number of drinking glasses
+        int nGlasses=6;
+        //Cost per glass
+        double glassCost$=2.29;
+        double glasscostbeforetax$=2.29*6; //glass cost before tax
+        double glasscostwithtax$=2.29*.06; //glass cost with tax
+
+        
+        //Number of boxes of envelopes
+        int nEnvelopes=1;
+        //cost per box of envelopes
+        double envelopeCost$=3.25;
+        double taxPercent=0.06;
+        double enevelopecostbeforetax$=3.25*1; //enevelope cost before tax
+        double envelopecostwithtax$=3.25*.06; //envelope cost with tax
+  }
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
