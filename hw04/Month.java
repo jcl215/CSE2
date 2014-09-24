f0e2b64e25221af4c90f32253684f50ee7b6d6ca
jcl215 <jcl215@lehigh.edu>
2014-09-24 02:57:26 +0000

    upload

---
+++ hw04/Month.java
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////
+//Jessica Lazo Quintana
+//CSE2
+//
+//Compile then run the program IncomeTax.java
+//
+import java.util.Scanner; //Scanner class import
+//
+// define a class
+public class BigMac {
+    
+//  add main method
+    public static void main(String[] args) {
+        Enter an int giving the number of the month (1-12)- 7
+        The month has 31 days
+         
+        Enter an int giving the number of the month (1-12)- 4
+        The month has 30 days
+         
+        Enter an int giving the number of the month (1-12)- 2
+        Enter an int giving the year 2000
+        The month has 29 days
+         
+        Enter an int giving the number of the month (1-12)- 2
+        Enter an int giving the year 1900
+        The month has 28 days
+         
+        Enter an int giving the number of the month (1-12)- 13
+        You did not enter an int between 1 and 12
+                            
+    } //end of main method
+} //end of class
